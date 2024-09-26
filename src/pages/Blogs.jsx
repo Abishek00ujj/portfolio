b9ba3ebe1380c5a3ce7e684d5d1bf2b45d2847eb
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-09-26 21:51:22 +0530

    wr

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect, Suspense } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Loader } from 'lucide-react';
 import { Navbar } from '../components/Navbar';
 import BlogComponent from '../components/BlogComponent';
+
 export const Blogs = () => {
   const url = "https://66e527045cc7f9b6273c6d1d.mockapi.io/Blogs";
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
+    setLoading(true);
     try {
       const response = await axios.get(url);
       if (response.status === 200 || response.status === 201) {
@@ -19,7 +21,7 @@ export const Blogs = () => {
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
@@ -33,7 +35,7 @@ export const Blogs = () => {
       <div className='w-screen h-auto flex flex-col justify-center items-center'>
         <h1 className='text-4xl text-orange-400 font-mono'>BLOGS</h1>
         {loading ? (
-          <Loader className='text-white animate-spin' /> // Loading indicator
+          <Loader className='text-white animate-spin' />
         ) : blogs.length > 0 ? (
           blogs.map((item) => (
             <BlogComponent
