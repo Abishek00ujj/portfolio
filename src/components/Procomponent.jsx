573fa7cef171ff303d18d0a30f86270c6593bb0e
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-12-01 19:11:54 +0530

    Update Procomponent.jsx

--- src/components/Procomponent.jsx
+++ src/components/Procomponent.jsx
@@ -45,7 +45,7 @@ export const Procomponent = (props) => {
             View Project <Github className="ml-2" />
           </a>
           <a
-            href={props.livelink || props.gitlink}
+            href={props.previewlink || props.previewlink}
             target="_blank"
             rel="noopener noreferrer"
             className="bg-white text-black flex items-center justify-center px-4 py-2 rounded-md font-semibold shadow-md hover:bg-gray-200">
